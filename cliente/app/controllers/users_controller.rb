05d62e486aeca94138335ca7969b35d09c4d2fc7
es lindo amar y ser amado <Moises Pablo Calderon>
2021-11-21 22:57:56 -0500

    proyecto inicial

---
+++ cliente/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new		
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			return redirect_to new_users_path
+		else
+			render :new
+			
+		end
+		
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :modafemenina, :modamasculina, :modainfantil)
+		
+	end
+end
---
+++ cliente/db/migrate/20211121030253_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.boolean :modafemenina
+      t.boolean :modamasculina
+      t.boolean :modainfantil
+
+      t.timestamps
+    end
+  end
+end
